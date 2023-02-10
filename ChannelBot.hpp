875c1b62aa08d1a7fdaef7d53c1f58d15ae11847
Kewin Hatlas <kewin.hatlas@gmail.com>
2023-02-10 17:17:08 +0100

    last push before some crazy multithreading

--- ChannelBot.hpp
+++ ChannelBot.hpp
@@ -2,24 +2,24 @@
 # define CHANNELBOT_HPP
 
 # include "Channel.hpp"
+# include <ctime>
 
 class ChannelBot
 {
 private:
-	std::map<std::string, privilege>
-	/* data */
+	Channel								*_chan;	//ChannelBot is contained within this channel, so this may be bad practice but I'm not sure how else to do this
+	std::map<std::string, privilege>	_users;
+	bool								_decay;
+	std::time_t							_expiry;	//every second I will have my Channels check whether the bot should be destroyed, so I should maybe use multiple threads
 public:
-	ChannelBot(/* args */);
+	ChannelBot(Channel *chan);
 	~ChannelBot();
+	int		checkUser(std::string hostmask);
+	void	toggleDecay();
+	bool	checkExpired();
+	void	addUser(std::string hostmask, privilege priv);
+	void	inviteUser(std::string hostmask);
+	// I'll have the destructor manage this, in Channel the ChannelBot will be a ChannelBot pointer, and it will be allocated for on the heap, then deleted when no longer needed
 };
-
-ChannelBot::ChannelBot(/* args */)
-{
-}
-
-ChannelBot::~ChannelBot()
-{
-}
-
 
 #endif
